c2b1f7a04bce24a04dbe3eb7ab3aeb8efe636fd1
Rowan Mullin <rmullin@smail.pembinatrails.ca>
2021-04-08 12:51:25 -0500

    Added new function for getting champion mastery.

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const SUMMONER_URL = `/lol/summoner/v4/summoners/by-name/`;
 const temp_key = `RGAPI-103fd9dd-67ad-4c89-8bd0-59fed16383aa`;
 const END_URL = `?api_key=` + temp_key;
 
-async function getChampionMastery() {
-  const res = await fetch(BASE_URL + SUMMONER_URL + `testname` + END_URL, {
-		method: `GET`,
-  });
+const summoner_url = `https://api.ttmhgame20.repl.co/getsummoner?name=`;
+const mastery_url = `https://api.ttmhgame20.repl.co/getmastery?id=`;
 
+async function getSummoner() {
+  const res = await fetch(summoner_url + `testname`);
 	const data = await res.json();
-	console.log(data);
+	getChampionMastery(data.id);
 }
 
-getChampionMastery();
+async function getChampionMastery(id) {
+	const res = await fetch(mastery_url + id);
+	const data = await res.json();
+}
+
+getSummoner();
