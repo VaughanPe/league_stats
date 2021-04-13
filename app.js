588c9807d9ae151d4a9f472e0d26548c57bd0ebb
Caelan Dubois <ttMHgame20@gmail.com>
2021-04-12 20:57:39 -0500

    completed the matches part of the site and fixed a few bugs with the chart

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const form = document.querySelector(`form`);
 const searchBar = document.querySelector(`input`);
 const championMasteryDisplay = document.querySelector(`ol`);
-
+const recentMatches = document.querySelector(".match-holder");
 const summoner_url = `https://api.ttmhgame20.repl.co/getsummoner?name=`;
 const mastery_url = `https://api.ttmhgame20.repl.co/getmastery?id=`;
+const graph = document.querySelector(".match");
 const championStats_url = `http://ddragon.leagueoflegends.com/cdn/11.7.1/data/en_US/champion.json`;
 const champIcon_url = `http://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/`;
-
+let players = [1,2,3,4,5,6,7,8,9,10]
+let globalM;
+let accountId;
+let myChart = "";
 async function getSummoner(name) {
   const res = await fetch(summoner_url + name);
 	const data = await res.json();
 	if (res.status !== 200) {
 		displayTopChamps([]);
 	} else {
-		getChampionMastery(data.id);
+        getChampionMastery(data.id);
+        accountId = data.accountId
+        getRecentMatches(data.accountId);
 	}
 }
 
@@ -73,29 +79,88 @@ form.addEventListener(`submit`, e => {
 	getSummoner(searchBar.value);
 });
 
+recentMatches.addEventListener('click', function(e){
+    if(e.target.tagName == "LI"){
+        console.log(e.target.id)
+        stats(globalM.matches[e.target.id].gameId,accountId);
+    }
+})
 
+async function getRecentMatches(accId){
+    let recent = await fetch(`https://api.ttmhgame20.repl.co/getmatches?accId=${accId}`);
+    let response = await recent.json();
+    globalM = response;
+    let champ = await fetch(championStats_url)
+    let res = await champ.json();
+    let champList = [];
+    let maps = await fetch(`http://static.developer.riotgames.com/docs/lol/maps.json`);
+    let mresp = await maps.json();
+    for(let item in res.data){
+            console.log(item);
+          if(res.data.hasOwnProperty(item)){
+              champList.push(res.data[item]);
+           }
+        }
+
+        
+    
+     recentMatches.innerHTML = ``;
+    for(let i =0;i <= 5;i++){
+        let match = await fetch(`https://api.ttmhgame20.repl.co/getmatch?matchId=${response.matches[i].gameId}`)
+        let matchresp = await match.json();
+        let image = "";
+        let id = "";
+        let Pid;
+        let stats;
+    
+
+        champList.forEach(function(element){
+            if(response.matches[i].champion == element.key){
+                image = `${champIcon_url}${element.key}.png`
+                id = element.id;
+            }
+        });
+        matchresp.participantIdentities.forEach(function(e){
+            if(e.player.accountId == accId){
+                Pid = e.participantId -1;
+                stats = matchresp.participants[Pid].stats;
+                console.log(matchresp.participants[Pid])
+            }
+        })
+        
+      recentMatches.insertAdjacentHTML(`beforeend`,`
+     <li class="match" id=${i}>
+     <img src="${image}" alt="${id}">
+     <h5>${matchresp.gameMode}</h5>
+     <h5>${stats.kills}/${stats.deaths}/${stats.assists}</h5>
+     <h5>${stats.totalMinionsKilled}</h5>
+    </li>`)
+    }
+
+}
 
 
 // stats method requires match id and accId
-stats("3853978668","NleIX8LLH1X3n9UB632uwKmaERhWGR9eaLdA-SE7a1dk9g");
- let deathsArray = [];
- let assistsArray = [];
+
+ 
+ 
 
   async function stats(m,acc){
- let time = [];
+    
+  let time = [];
   let gold = [];
   let gold2 = [];
   let cs  = [];
   let kills = 0;
   let killsArray = [];
   let deaths = 0;
-  
+  let deathsArray = [];
   let assists = 0;
-  
+  let assistsArray = [];
   let Pid;
-  let players = [1,2,3,4,5,6,7,8,9,10]
+  
 
-let match = await fetch("https://api.ttmhgame20.repl.co/getmatch?matchId=3853978668");
+let match = await fetch(`https://api.ttmhgame20.repl.co/getmatch?matchId=${m}`);
 let resp = await match.json();
 
 resp.participantIdentities.forEach(function(id){
@@ -103,7 +168,7 @@ resp.participantIdentities.forEach(function(id){
         Pid = id.participantId;
     }
 })
-let matcht = await fetch("https://api.ttmhgame20.repl.co/gettimeline?matchId=3853978668")
+let matcht = await fetch(`https://api.ttmhgame20.repl.co/gettimeline?matchId=${m}`)
 let tresp = await matcht.json();
 let counter = 0;
       tresp.frames.forEach(element => {
@@ -118,10 +183,12 @@ let counter = 0;
                       kills++;
                   } else if(e.type == "CHAMPION_KILL" && e.victimId == Pid){
                         deaths++;
-                  } else if(e.type == "CHAMPION_KILL" && e.assistingParticipantIds.leangth >= 1){
+                  } else if(e.type == "CHAMPION_KILL" && e.assistingParticipantIds.length >= 1){
                       e.assistingParticipantIds.forEach(function(e){
                           if(e == Pid){
+                              
                               assists++;
+                              console.log(assists)
                           }
                       })
                   }
@@ -147,12 +214,10 @@ let counter = 0;
 
 
 var ctx = document.getElementById('match').getContext('2d');
+if(myChart != ""){
+myChart.destroy();
 
-
-
-
-
-var myChart = new Chart(ctx, {
+myChart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: time,
@@ -184,14 +249,14 @@ var myChart = new Chart(ctx, {
           {
             label: 'Deaths',
             data: deathsArray,
-            borderColor: "#eb4034",
-            backgroundColor: "#e37f78",
+            borderColor: "#000000",
+            backgroundColor: "#000000",
           },
           {
             label: 'Assists',
             data: assistsArray,
-            borderColor: "#eb4034",
-            backgroundColor: "#e37f78",
+            borderColor: "#787878",
+            backgroundColor: "#999999",
           },
           
         ]
@@ -209,7 +274,68 @@ var myChart = new Chart(ctx, {
       }
     },
   });
+} else {
 
+
+
+ myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: time,
+        datasets: [
+          {
+            label: 'Total Gold',
+            data: gold,
+            borderColor: "#0d49b8",
+            backgroundColor: "#2353ad",
+          },
+          {
+            label: 'Current Gold',
+            data: gold2,
+            borderColor: "#ffff3b",
+            backgroundColor: "#ffff3b",
+          },
+          {
+            label: 'CS',
+            data: cs,
+            borderColor: "#64d9a2",
+            backgroundColor: "#89d9b3",
+          },
+          {
+            label: 'Kills',
+            data: killsArray,
+            borderColor: "#8b0000",
+            backgroundColor: "#7a0000",
+          },
+          {
+            label: 'Deaths',
+            data: deathsArray,
+            borderColor: "#000000",
+            backgroundColor: "#000000",
+          },
+          {
+            label: 'Assists',
+            data: assistsArray,
+            borderColor: "#787878",
+            backgroundColor: "#999999",
+          },
+          
+        ]
+      },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'LOL Match Stats'
+        }
+      }
+    },
+  });
+}
 
   
 }
