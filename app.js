526c8242d097a063d7043951c335586a84300c9e
Caelan Dubois <ttMHgame20@gmail.com>
2021-04-13 08:35:12 -0500

    fixed graph not counting jungle cs

--- app.js
+++ app.js
@@ -7,86 +7,87 @@ const mastery_url = `https://api.ttmhgame20.repl.co/getmastery?id=`;
 const graph = document.querySelector(".match");
 const championStats_url = `http://ddragon.leagueoflegends.com/cdn/11.7.1/data/en_US/champion.json`;
 const champIcon_url = `http://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/`;
-let players = [1,2,3,4,5,6,7,8,9,10]
+let players = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 let globalM;
 let accountId;
 let myChart = "";
+
 async function getSummoner(name) {
-  const res = await fetch(summoner_url + name);
-	const data = await res.json();
-	if (res.status !== 200) {
-		displayTopChamps([]);
-	} else {
+    const res = await fetch(summoner_url + name);
+    const data = await res.json();
+    if (res.status !== 200) {
+        displayTopChamps([]);
+    } else {
         getChampionMastery(data.id);
         accountId = data.accountId
         getRecentMatches(data.accountId);
-	}
+    }
 }
 
 async function getChampionMastery(id) {
-	const res = await fetch(mastery_url + id);
-	const data = await res.json();
-	getTopChamps(data);
+    const res = await fetch(mastery_url + id);
+    const data = await res.json();
+    getTopChamps(data);
 }
 
 function getTopChamps(champArray) {
-	let newChampArray = [];
-	let counter = 1;
-	for (let i = 0; i < 3; i++) {
-		if (champArray.length > counter) {
-			newChampArray.push(champArray[i]);
-			counter++;
-		}
-	}
+    let newChampArray = [];
+    let counter = 1;
+    for (let i = 0; i < 3; i++) {
+        if (champArray.length > counter) {
+            newChampArray.push(champArray[i]);
+            counter++;
+        }
+    }
 
-	getChampStats(newChampArray);
+    getChampStats(newChampArray);
 }
 
 async function getChampStats(champArray) {
-	const res = await fetch(championStats_url);
-	const data = await res.json();
-	let newChampArray = [];
-	champArray.forEach(champ => {
-		for (const champData in data.data) {
-			if (champ.championId === parseInt(data.data[champData].key)) {
-				newChampArray.push({champ, champData: data.data[champData]})
-			}
-		}
-	});
+    const res = await fetch(championStats_url);
+    const data = await res.json();
+    let newChampArray = [];
+    champArray.forEach(champ => {
+        for (const champData in data.data) {
+            if (champ.championId === parseInt(data.data[champData].key)) {
+                newChampArray.push({ champ, champData: data.data[champData] })
+            }
+        }
+    });
 
-	displayTopChamps(newChampArray);
+    displayTopChamps(newChampArray);
 }
 
 function displayTopChamps(champArray) {
-	console.log(champArray);
-	championMasteryDisplay.innerHTML = ``;
-	champArray.forEach( champ => {
-		let name = champ.champData.id
-		let img = champIcon_url + champ.champData.key + `.png`;
-		let mp = champ.champ.championPoints
-		championMasteryDisplay.insertAdjacentHTML(`beforeend`, `
+    console.log(champArray);
+    championMasteryDisplay.innerHTML = ``;
+    champArray.forEach(champ => {
+        let name = champ.champData.id
+        let img = champIcon_url + champ.champData.key + `.png`;
+        let mp = champ.champ.championPoints
+        championMasteryDisplay.insertAdjacentHTML(`beforeend`, `
 		<li>
 			<img src="${img}" alt="Champion">
 			<h3>${name}</h3>
 			<h5>Mastery Points: ${mp}</h4>
 		</li>
 		`);
-	});
+    });
 }
 
 form.addEventListener(`submit`, e => {
-	e.preventDefault();
-	getSummoner(searchBar.value);
+    e.preventDefault();
+    getSummoner(searchBar.value);
 });
 
-recentMatches.addEventListener('click', function(e){
-    if(e.target.tagName == "LI"){
+recentMatches.addEventListener('click', function (e) {
+    if (e.target.tagName == "LI") {
         console.log(e.target.id)
-        stats(globalM.matches[e.target.id].gameId,accountId);
+        stats(globalM.matches[e.target.id].gameId, accountId);
     }
 })
 
-async function getRecentMatches(accId){
+async function getRecentMatches(accId) {
     let recent = await fetch(`https://api.ttmhgame20.repl.co/getmatches?accId=${accId}`);
     let response = await recent.json();
     globalM = response;
@@ -95,45 +96,45 @@ async function getRecentMatches(accId){
     let champList = [];
     let maps = await fetch(`http://static.developer.riotgames.com/docs/lol/maps.json`);
     let mresp = await maps.json();
-    for(let item in res.data){
-            console.log(item);
-          if(res.data.hasOwnProperty(item)){
-              champList.push(res.data[item]);
-           }
+    for (let item in res.data) {
+        console.log(item);
+        if (res.data.hasOwnProperty(item)) {
+            champList.push(res.data[item]);
         }
+    }
 
-        
-    
-     recentMatches.innerHTML = ``;
-    for(let i =0;i <= 5;i++){
+
+
+    recentMatches.innerHTML = ``;
+    for (let i = 0; i <= 5; i++) {
         let match = await fetch(`https://api.ttmhgame20.repl.co/getmatch?matchId=${response.matches[i].gameId}`)
         let matchresp = await match.json();
         let image = "";
         let id = "";
         let Pid;
         let stats;
-    
 
-        champList.forEach(function(element){
-            if(response.matches[i].champion == element.key){
+
+        champList.forEach(function (element) {
+            if (response.matches[i].champion == element.key) {
                 image = `${champIcon_url}${element.key}.png`
                 id = element.id;
             }
         });
-        matchresp.participantIdentities.forEach(function(e){
-            if(e.player.accountId == accId){
-                Pid = e.participantId -1;
+        matchresp.participantIdentities.forEach(function (e) {
+            if (e.player.accountId == accId) {
+                Pid = e.participantId - 1;
                 stats = matchresp.participants[Pid].stats;
                 console.log(matchresp.participants[Pid])
             }
         })
-        
-      recentMatches.insertAdjacentHTML(`beforeend`,`
+
+        recentMatches.insertAdjacentHTML(`beforeend`, `
      <li class="match" id=${i}>
      <img src="${image}" alt="${id}">
      <h5>${matchresp.gameMode}</h5>
      <h5>${stats.kills}/${stats.deaths}/${stats.assists}</h5>
-     <h5>${stats.totalMinionsKilled}</h5>
+     <h5>${stats.neutralMinionsKilled + stats.totalMinionsKilled}</h5>
     </li>`)
     }
 
@@ -142,200 +143,201 @@ async function getRecentMatches(accId){
 
 // stats method requires match id and accId
 
- 
- 
 
-  async function stats(m,acc){
-    
-  let time = [];
-  let gold = [];
-  let gold2 = [];
-  let cs  = [];
-  let kills = 0;
-  let killsArray = [];
-  let deaths = 0;
-  let deathsArray = [];
-  let assists = 0;
-  let assistsArray = [];
-  let Pid;
-  
 
-let match = await fetch(`https://api.ttmhgame20.repl.co/getmatch?matchId=${m}`);
-let resp = await match.json();
 
-resp.participantIdentities.forEach(function(id){
-    if(id.player.accountId == acc){
-        Pid = id.participantId;
+async function stats(m, acc) {
+
+    let time = [];
+    let gold = [];
+    let gold2 = [];
+    let cs = [];
+    let kills = 0;
+    let killsArray = [];
+    let deaths = 0;
+    let deathsArray = [];
+    let assists = 0;
+    let assistsArray = [];
+    let Pid;
+
+
+    let match = await fetch(`https://api.ttmhgame20.repl.co/getmatch?matchId=${m}`);
+    let resp = await match.json();
+
+    resp.participantIdentities.forEach(function (id) {
+        if (id.player.accountId == acc) {
+            Pid = id.participantId;
+        }
+    })
+    let matcht = await fetch(`https://api.ttmhgame20.repl.co/gettimeline?matchId=${m}`)
+    let tresp = await matcht.json();
+    let counter = 0;
+    tresp.frames.forEach(element => {
+        time.push(`${counter}`)
+        counter++;
+
+        element.events.forEach(function (e) {
+
+
+            if (e.type == "CHAMPION_KILL" && e.killerId == Pid) {
+                console.log(kills)
+                kills++;
+            } else if (e.type == "CHAMPION_KILL" && e.victimId == Pid) {
+                deaths++;
+            } else if (e.type == "CHAMPION_KILL" && e.assistingParticipantIds.length >= 1) {
+                e.assistingParticipantIds.forEach(function (e) {
+                    if (e == Pid) {
+
+                        assists++;
+                        console.log(assists)
+                    }
+                })
+            }
+
+
+        })
+        killsArray.push(`${kills}`);
+        deathsArray.push(`${deaths}`);
+        assistsArray.push(`${assists}`)
+        players.forEach(function (e) {
+
+            if (element.participantFrames[e].participantId == Pid) {
+                gold.push(element.participantFrames[e].totalGold);
+                console.log(element.participantFrames[e].jungleMinionsKilled);
+                cs.push(element.participantFrames[e].minionsKilled + element.participantFrames[e].jungleMinionsKilled);
+                gold2.push(element.participantFrames[e].currentGold);
+
+            }
+        })
+
+
+    });
+
+
+
+    var ctx = document.getElementById('match').getContext('2d');
+    if (myChart != "") {
+        myChart.destroy();
+
+        myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: time,
+                datasets: [
+                    {
+                        label: 'Total Gold',
+                        data: gold,
+                        borderColor: "#0d49b8",
+                        backgroundColor: "#2353ad",
+                    },
+                    {
+                        label: 'Current Gold',
+                        data: gold2,
+                        borderColor: "#ffff3b",
+                        backgroundColor: "#ffff3b",
+                    },
+                    {
+                        label: 'CS',
+                        data: cs,
+                        borderColor: "#64d9a2",
+                        backgroundColor: "#89d9b3",
+                    },
+                    {
+                        label: 'Kills',
+                        data: killsArray,
+                        borderColor: "#8b0000",
+                        backgroundColor: "#7a0000",
+                    },
+                    {
+                        label: 'Deaths',
+                        data: deathsArray,
+                        borderColor: "#000000",
+                        backgroundColor: "#000000",
+                    },
+                    {
+                        label: 'Assists',
+                        data: assistsArray,
+                        borderColor: "#787878",
+                        backgroundColor: "#999999",
+                    },
+
+                ]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'LOL Match Stats'
+                    }
+                }
+            },
+        });
+    } else {
+
+
+
+        myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: time,
+                datasets: [
+                    {
+                        label: 'Total Gold',
+                        data: gold,
+                        borderColor: "#0d49b8",
+                        backgroundColor: "#2353ad",
+                    },
+                    {
+                        label: 'Current Gold',
+                        data: gold2,
+                        borderColor: "#ffff3b",
+                        backgroundColor: "#ffff3b",
+                    },
+                    {
+                        label: 'CS',
+                        data: cs,
+                        borderColor: "#64d9a2",
+                        backgroundColor: "#89d9b3",
+                    },
+                    {
+                        label: 'Kills',
+                        data: killsArray,
+                        borderColor: "#8b0000",
+                        backgroundColor: "#7a0000",
+                    },
+                    {
+                        label: 'Deaths',
+                        data: deathsArray,
+                        borderColor: "#000000",
+                        backgroundColor: "#000000",
+                    },
+                    {
+                        label: 'Assists',
+                        data: assistsArray,
+                        borderColor: "#787878",
+                        backgroundColor: "#999999",
+                    },
+
+                ]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'LOL Match Stats'
+                    }
+                }
+            },
+        });
     }
-})
-let matcht = await fetch(`https://api.ttmhgame20.repl.co/gettimeline?matchId=${m}`)
-let tresp = await matcht.json();
-let counter = 0;
-      tresp.frames.forEach(element => {
-          time.push(`${counter}`)
-          counter++;
-
-          element.events.forEach(function (e){
-              
-             
-                  if(e.type == "CHAMPION_KILL" && e.killerId == Pid){
-                      console.log(kills)
-                      kills++;
-                  } else if(e.type == "CHAMPION_KILL" && e.victimId == Pid){
-                        deaths++;
-                  } else if(e.type == "CHAMPION_KILL" && e.assistingParticipantIds.length >= 1){
-                      e.assistingParticipantIds.forEach(function(e){
-                          if(e == Pid){
-                              
-                              assists++;
-                              console.log(assists)
-                          }
-                      })
-                  }
-              
-               
-          })
-               killsArray.push(`${kills}`);
-               deathsArray.push(`${deaths}`);
-               assistsArray.push(`${assists}`)
-          players.forEach(function(e){
-
-              if(element.participantFrames[e].participantId == Pid){
-                  gold.push(element.participantFrames[e].totalGold);
-                  cs.push(element.participantFrames[e].minionsKilled);
-                  gold2.push(element.participantFrames[e].currentGold);
-                  
-              }
-          })
-
-          
-      });
 
 
-
-var ctx = document.getElementById('match').getContext('2d');
-if(myChart != ""){
-myChart.destroy();
-
-myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: time,
-        datasets: [
-          {
-            label: 'Total Gold',
-            data: gold,
-            borderColor: "#0d49b8",
-            backgroundColor: "#2353ad",
-          },
-          {
-            label: 'Current Gold',
-            data: gold2,
-            borderColor: "#ffff3b",
-            backgroundColor: "#ffff3b",
-          },
-          {
-            label: 'CS',
-            data: cs,
-            borderColor: "#64d9a2",
-            backgroundColor: "#89d9b3",
-          },
-          {
-            label: 'Kills',
-            data: killsArray,
-            borderColor: "#8b0000",
-            backgroundColor: "#7a0000",
-          },
-          {
-            label: 'Deaths',
-            data: deathsArray,
-            borderColor: "#000000",
-            backgroundColor: "#000000",
-          },
-          {
-            label: 'Assists',
-            data: assistsArray,
-            borderColor: "#787878",
-            backgroundColor: "#999999",
-          },
-          
-        ]
-      },
-    options: {
-      responsive: true,
-      plugins: {
-        legend: {
-          position: 'top',
-        },
-        title: {
-          display: true,
-          text: 'LOL Match Stats'
-        }
-      }
-    },
-  });
-} else {
-
-
-
- myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: time,
-        datasets: [
-          {
-            label: 'Total Gold',
-            data: gold,
-            borderColor: "#0d49b8",
-            backgroundColor: "#2353ad",
-          },
-          {
-            label: 'Current Gold',
-            data: gold2,
-            borderColor: "#ffff3b",
-            backgroundColor: "#ffff3b",
-          },
-          {
-            label: 'CS',
-            data: cs,
-            borderColor: "#64d9a2",
-            backgroundColor: "#89d9b3",
-          },
-          {
-            label: 'Kills',
-            data: killsArray,
-            borderColor: "#8b0000",
-            backgroundColor: "#7a0000",
-          },
-          {
-            label: 'Deaths',
-            data: deathsArray,
-            borderColor: "#000000",
-            backgroundColor: "#000000",
-          },
-          {
-            label: 'Assists',
-            data: assistsArray,
-            borderColor: "#787878",
-            backgroundColor: "#999999",
-          },
-          
-        ]
-      },
-    options: {
-      responsive: true,
-      plugins: {
-        legend: {
-          position: 'top',
-        },
-        title: {
-          display: true,
-          text: 'LOL Match Stats'
-        }
-      }
-    },
-  });
-}
-
-  
 }
